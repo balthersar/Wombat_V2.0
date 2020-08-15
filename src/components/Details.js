f95c48fe15adb37c671cc1d4d590902e3f7e7e2e
Dominik Jürgens <juergensdj@gmx.de>
2020-08-15 16:57:51 +0200

    add wombat products instead of phones, add available/not available products

--- src/components/Details.js
+++ src/components/Details.js
@@ -2,14 +2,17 @@ import React, { Component } from 'react';
 import {ProductConsumer} from '../context';
 import {Link} from 'react-router-dom';
 import {ButtonContainer} from './Button';
+import styling from 'styled-components';
 
 export default class Details extends Component {
     render() {
         return (
+            <DetailWrapper>
             <ProductConsumer>
                 {(value) => {
-                    const {id, company, img, info, price, title, inCart} = value.detailProduct;
+                    const {id, company, img, info, price, title, inCart, available} = value.detailProduct;
                     return (
+                        <div className="py-2">
                         <div className="container py-5">
                             {/*title */}
                             <div className = "row">
@@ -29,19 +32,25 @@ export default class Details extends Component {
                                         made by: <span className="text-uppercase">{company}</span>
                                     </h4>
                                     <h4 className = "text-blue">
-                                        <strong>price: {price}<span>€</span></strong>
+                                        <strong>Preis: {price}<span>€</span></strong>
                                     </h4>
                                     <p className="text-capitalize font-weight-bold mt-3 mb-3">
-                                        some info about product:
+                                        Weitere Informationen:
                                     </p>
                                     <p className="text-muted lead">{info}</p>
+                                    {/* Model ist derzeit ausverkauft */}
+                                    {available ?(
+                                        null
+                                        ):<p className="text-red font-italic mb-0"> Derzeit leider nicht verfügbar </p>
+                                    }
                                     {/*buttons */}
                                     <div>
                                         <Link to='/'>
                                             <ButtonContainer>
-                                                Back to Products
+                                                Zurück 
                                             </ButtonContainer>
                                         </Link>
+                                        {available ?(
                                         <ButtonContainer 
                                             cart
                                             disabled={inCart?true:false}
@@ -50,16 +59,28 @@ export default class Details extends Component {
                                                 //value.openModal(id);
                                             }}
                                         >
-                                            {inCart? 'inCart':"add to Cart"}
+                                            {inCart? 'Im Warenkorb':"In den Warenkorb"}
                                         </ButtonContainer>
+                                        ):null}
                                     </div>
                                 </div>
                             </div>
+                        </div>
                         </div>
                     )
                 }}
                 
             </ProductConsumer>
+            </DetailWrapper>
         )
     }
 }
+const DetailWrapper = styling.nav`
+.container{
+    border-radius: 10px;
+    background-color:rgba(0, 0, 0, 0.65);
+    color:white;
+    
+    z-index: -99;
+}
+`;
--- src/components/Product.js
+++ src/components/Product.js
@@ -6,19 +6,21 @@ import PropTypes from 'prop-types';
 
 export default class Product extends Component {
     render() {
-        const {id, title, img, price, inCart} = this.props.product;
+        const {id, title, img, price, inCart, available} = this.props.product;
         return (
             <ProductWrapper className="col-9 mx-auto col-md-6 col-lg-3 my-3" >
                 <div className="card">
                     <ProductConsumer>
                     {/* Card Image */}
                     {value => (
-                        <div className="img-container p-5" 
+                        <div className="img-container p-3" 
                         onClick={() => value.handleDetail(id)}
                         >
                             <Link to="/details">
                                 <img src={img} alt ="product" className="card-img-top" />
                             </Link>
+                            {/* Button warenkorb nur bei verfügbaren Artikel */}
+                            {available ?(
                             <button 
                             className="cart-btn"
                             disabled = {inCart ? true : false}
@@ -36,16 +38,24 @@ export default class Product extends Component {
                                         <i className=" fas fa-cart-plus"/>
                                     )} 
                             </button>
+                            ):null}
                         </div>
                     )}
                     
                     </ProductConsumer>
+                    
+                    
+                    
                     {/* Card Footer*/}
                     <div className="card-footer d-flex justify-content-between">
-                        <p className="align-self-center mb-0">
+                        <p className="text-blue font-itaic align-self-center mb-0">
                             {title}
                         </p>
-                        <h5 className="text-blue font-itaic mb-0">
+                        {available ?(
+                            null
+                        ):<p className="text-red font-italic mb-0"> Ausverkauft</p>};
+                    
+                        <h5 className="text-blue font-italic mb-0">
                             <span className="mr-1">€</span>
                             {price}
                         </h5>
@@ -90,17 +100,17 @@ const ProductWrapper = styled.div`
         overflow:hidden;
     }
     .card-img-top{
-        transition:all 0.5s linear;
+        transition:all 0.2s linear;
     }
     .img-container:hover .card-img-top{
-        transform:scale(1.2)
+        transform:scale(1.1)
     }
     .cart-btn{
         position:absolute;
         bottom:0;
         right:0;
         padding:0.2rem 0.4rem;
-        background:var(--lightBlue);
+        background:var(--wombatGreen);
         border:none;
         color:var(--mainWhite);
         font-size:1.4rem;
